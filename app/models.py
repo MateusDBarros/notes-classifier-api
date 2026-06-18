0dd87f09b42b2fec5620079a32c957635064f43d
Mateus Barros <mb685212@gmail.com>
2026-06-18 13:51:30 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,9 +1,8 @@
 
-from datetime import date
+from datetime import datetime
 from typing import Optional
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped
-from sqlalchemy.testing.schema import mapped_column
+from sqlalchemy import String, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
 
 from .database import Base
 
@@ -13,5 +12,5 @@ class Note(Base):
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     title: Mapped[str] = mapped_column(String(80))
     content: Mapped[str] = mapped_column(String(225))
-    tag: Mapped[str] = mapped_column(String(225))
-    created_at: Mapped[Optional[date]] = mapped_column() # optional but good practice
+    tag: Mapped[Optional[str]] = mapped_column(String(225), nullable=True)
+    created_at: Mapped[Optional[datetime]] = mapped_column(DateTime, server_default=func.now()) # optional but good practice
