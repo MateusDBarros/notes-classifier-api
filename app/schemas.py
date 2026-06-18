27411b36ebafed7b8062da4ed8277270a22f0d49
Mateus Barros <mb685212@gmail.com>
2026-06-18 13:51:34 -0300

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -8,11 +8,14 @@ class NoteCreate(BaseModel):
     content: str
 
 class NoteUpdate(BaseModel):
+    title: str = None
+    content: str = None
+    tag: Optional[str] = None
+
+class NoteResponse(BaseModel):
     id: int
     title: str
     content: str
     tag: Optional[str] = None
 
-    model_config = {
-        'from_atributes': True
-    }
+    model_config = {'from_attributes': True}
