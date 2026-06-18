8f69c002fc6fa6020a9c7477cd89badd0b46b058
Mateus Barros <mb685212@gmail.com>
2026-06-18 13:43:46 -0300

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings
+
+class Config(BaseSettings):
+
+    debug_tool: bool = False
+    db_user: str = ''
+    db_password: str = ''
+    db_name: str = 'notes.db'
+
+
+    @property
+    def db_url(self):
+        return f"sqlite:///./{self.db_name}"
+
+config = Config()
