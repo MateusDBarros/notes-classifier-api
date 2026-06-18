e6435166a560bb0d57d5e415cd3b541bd75e9cc0
Mateus Barros <mb685212@gmail.com>
2026-06-18 13:51:39 -0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from config import config
 
-engine = create_engine(config.db_name, connect_args={'check_same_thread': False}, echo=True)
+engine = create_engine(config.db_url, connect_args={'check_same_thread': False}, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
